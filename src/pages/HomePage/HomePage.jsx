31ea31c3ba40e3b7359ca8564faad7386c715bc0
Maxim Stribylo <max057521@gmail.com>
2022-08-28 13:11:21 +0300

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,39 @@
 import { useNavigate } from 'react-router-dom';
 import { ReactComponent as LogoReact } from '../HomePage/logoReact.svg';
-import { Rotate } from './HomePage.styled';
+import {
+  Box,
+  // Button,
+  // Typography
+} from '@mui/material';
+
+// import { Rotate } from './HomePage.styled';
 
 const HomePage = () => {
   const navigate = useNavigate();
 
   return (
     <>
-      <Rotate>
-        <LogoReact />
-      </Rotate>
-      <>Contacts phonebook from the React APP</>
-      <>Let's go!</>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <LogoReact
+          height="100"
+          title="LogoReact"
+          alt="LogoReact"
+          fill="#836bf2"
+          wrapperStyle={{
+            width: '100%',
+            animation: 'rotation 30s infinite linear',
+            transform: 'rotate(0deg, 359deg)',
+          }}
+        />
+      </Box>
+
+      <div>Contacts phonebook from the React APP</div>
+      <div>Let's go!</div>
       <button type="submit" onClick={() => navigate('/login')}>
         Try to now
       </button>
--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -3,13 +3,12 @@ import styled from '@emotion/styled';
 export const Rotate = styled.div`
   animation: rotation 30s infinite linear;
   width: 100%;
-}
-
-@keyframes rotation {
-  from {
-    transform: rotate(0deg);
+  @keyframes rotation {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(359deg);
+    }
   }
-  to {
-    transform: rotate(359deg);
-  }
-}`;
+`;
