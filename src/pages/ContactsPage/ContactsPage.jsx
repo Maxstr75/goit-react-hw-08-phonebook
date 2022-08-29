3365908dc7bc513013ff58b0d163b2bfb499659a
Maxim Stribylo <max057521@gmail.com>
2022-08-29 21:53:29 +0300

    fix bag

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,8 +1,11 @@
 import { Box, Typography } from '@mui/material';
+import ContactForm from '../../components/ContactForm/ContactForm';
+import ContactList from '../../components/ContactList/ContactList';
+import Filter from '../../components/Filter/Filter';
 
-const { ContactForm } = require('components/ContactForm');
-const { default: ContactList } = require('components/ContactList');
-const { default: Filter } = require('components/Filter');
+// const { ContactForm } = require('components/ContactForm');
+// const { default: ContactList } = require('components/ContactList');
+// const { default: Filter } = require('components/Filter');
 
 const ContactsPage = () => {
   return (
